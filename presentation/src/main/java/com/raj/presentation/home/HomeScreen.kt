2da1c17a58d0809048c9bc30af5c95108b6a6288
Raj Pratap Singh Jadon <>
2024-07-26 16:15:01 +0530

    add search

--- presentation/src/main/java/com/raj/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/raj/presentation/home/HomeScreen.kt
@@ -8,16 +8,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.raj.presentation.ui.component.ErrorUi
 import com.raj.presentation.ui.component.HomeScreenItem
@@ -26,39 +27,41 @@ import com.raj.presentation.ui.component.LoadingUi
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel, onClick: () -> Unit = {}) {
 
-    val data =
-        homeViewModel.pokemonList.collectAsState(initial = emptyList()).value
+    val data by homeViewModel.pokemonList.collectAsState(initial = emptyList())
+    val isLoading by homeViewModel.loading.collectAsState(initial = true)
+    val isError by homeViewModel.pokemonAppError.collectAsState(initial = null)
+    val searchQuery = remember { mutableStateOf(TextFieldValue("")) }
 
-    val isLoading = homeViewModel.loading.collectAsState(initial = true)
-    val isError = homeViewModel.pokemonAppError.collectAsState(initial = null)
+    val filteredItems = if (searchQuery.value.text.isEmpty()) {
+        data
+    } else {
+        data.filter { it.name?.contains(searchQuery.value.text, ignoreCase = true) == true }
+    }
 
     Column(Modifier.background(color = Color.Gray)) {
         Spacer(modifier = Modifier.statusBarsPadding())
-        var text by remember { mutableStateOf("") }
-        TextField(
-            value = text,
-            onValueChange = { text = it },
+        OutlinedTextField(
+            value = searchQuery.value,
+            onValueChange = { query ->
+                searchQuery.value = query
+            },
             label = { Text("Search") },
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(16.dp)
         )
 
-
-        if (isLoading.value)
-            LoadingUi()
-        else
-            isError.value?.let {
-                ErrorUi(pokemonAppError = it)
-            } ?: run {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(3),
-                    modifier = Modifier.padding(top = 10.dp)
-                ) {
-                    items(data.size) { position ->
-                        val pokemon = data[position]
-                        HomeScreenItem(pokemon, onClick = onClick)
-                    }
+        when {
+            isLoading -> LoadingUi()
+            isError != null -> isError?.let { ErrorUi(it) }
+            else -> LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier.padding(top = 10.dp)
+            ) {
+                items(filteredItems) { pokemon ->
+                    HomeScreenItem(pokemon, onClick = onClick)
                 }
             }
+        }
     }
 }
